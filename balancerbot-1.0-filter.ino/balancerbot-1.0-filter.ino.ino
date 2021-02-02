93e67a282165e226c5d09f5918608ff90a20ddfc
Kha <58523155+Cappu-kyani@users.noreply.github.com>
2021-02-02 20:01:31 +0500

    Add files via upload

---
+++ balancerbot-1.0-filter.ino/balancerbot-1.0-filter.ino.ino
@@ -0,0 +1,87 @@
+//libs
+#include "MPU6050.h"
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+#include "Wire.h"
+#endif
+
+//constants
+#define motorLF 26 //left motor, forward
+#define motorLB 27 //...backward
+#define motorLS 14 //...speed (PWM)
+#define motorRF 33 //right...
+#define motorRB 25 //...
+#define motorRS 32 //...
+
+//vars'n'objects
+MPU6050 accelgyro;//our accelerometer+gyro sensor we are going to use
+
+int16_t ax, ay, az;//we only going to use X and maybe Y, as it clearly shows an "UP" direction we gonna need...
+int16_t gx, gy, gz;//...and we ain't going to use any of these
+//because these only show rotation acceleration, but not "UP" or "DOWN" direction
+const int Pk = 2;
+void setup() {
+  Serial.begin(115200);
+  //set mode for motor direction control pins
+  pinMode(motorLF, OUTPUT);
+  pinMode(motorLB, OUTPUT);
+  pinMode(motorRF, OUTPUT);
+  pinMode(motorRB, OUTPUT);
+
+  //we might want to turn off the motors to not to mess things up
+  digitalWrite(motorLF, 0);
+  digitalWrite(motorLB, 0);
+  digitalWrite(motorRF, 0);
+  digitalWrite(motorRB, 0);
+
+  //initialize PWM channels
+  ledcSetup(0, 20000, 8);//left
+  ledcSetup(1, 20000, 8);//right
+  //connect PWM channels to corresponding motor speed control pins
+  ledcAttachPin(motorLS, 0);
+  ledcAttachPin(motorRS, 1);
+  //limit motor speed to 100% of max speed
+
+  //init i2c interface
+  Wire.begin(21, 22);
+  //then gyroaccel sensor
+  accelgyro.initialize();
+  //we ain't gonna use serial interface, because it slow af
+  //and it requires us to use WIRES, so we gonna use
+  //wifi instead (ofc later)
+  //btw, external bluetooth thing is considered because why not
+}
+
+void loop() {
+  
+  accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+  Serial.print(ax);
+  Serial.println("   ");
+  float refVal = regulator(ax);
+int PWR = map(abs(refVal), 0, 16000, 0, 255);
+//Serial.println(PWR);
+  ledcWrite(0, PWR);
+  ledcWrite(1, PWR);
+//  Serial.println(refVal);
+  if (ax < 100) {
+    digitalWrite(motorLF, 1);
+    digitalWrite(motorLB, 0);
+    digitalWrite(motorRF, 1);
+    digitalWrite(motorRB, 0);
+  } else if (ax > 100) {
+    digitalWrite(motorLF, 0);
+    digitalWrite(motorLB, 1);
+    digitalWrite(motorRF, 0);
+    digitalWrite(motorRB, 1);
+  } else {
+    digitalWrite(motorLF, 0);
+    digitalWrite(motorLB, 0);
+    digitalWrite(motorRF, 0);
+    digitalWrite(motorRB, 0);
+  }
+}
+//obtaining a refined value
+float regulator (int error)
+{
+  return error * Pk;
+}
+
