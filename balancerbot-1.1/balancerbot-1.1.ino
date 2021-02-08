74662576f43848a634cb6d285e8b369570c8023a
CaveJohnson376 <cavejohnson376@gmail.com>
2021-02-08 18:41:28 +0500

    code review, added stuff for later

--- balancerbot-1.1/balancerbot-1.1.ino
+++ balancerbot-1.1/balancerbot-1.1.ino
@@ -5,6 +5,7 @@
 #endif
 
 //constants
+//swap numbers in there if things are messed up
 #define motorLF 26 //left motor, forward
 #define motorLB 27 //...backward
 #define motorLS 14 //...speed (PWM)
@@ -12,15 +13,20 @@
 #define motorRB 25 //...
 #define motorRS 32 //...
 
+
+const float Pk = 2; //Proportional
+const float Ik = 0; //Integral
+const float Dk = 0; //Differential
+
 //vars'n'objects
 MPU6050 accelgyro;//our accelerometer+gyro sensor we are going to use
 
 int16_t ax, ay, az;//we only going to use X and maybe Y, as it clearly shows an "UP" direction we gonna need...
 int16_t gx, gy, gz;//...and we ain't going to use any of these
 //because these only show rotation acceleration, but not "UP" or "DOWN" direction
-const int Pk = 2;
+
+
 void setup() {
-  Serial.begin(115200);
   //set mode for motor direction control pins
   pinMode(motorLF, OUTPUT);
   pinMode(motorLB, OUTPUT);
@@ -52,36 +58,38 @@ void setup() {
 }
 
 void loop() {
-  
+
   accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-  Serial.print(ax);
-  Serial.println("   ");
-  float refVal = regulator(ax);
-int PWR = map(abs(refVal), 0, 16000, 0, 255);
-//Serial.println(PWR);
-  ledcWrite(0, PWR);
-  ledcWrite(1, PWR);
-//  Serial.println(refVal);
-  if (ax < 100) {
+  
+  float motion = PID(ax);
+  
+  int PWM = map(abs(motion), 0, 16000, 0, 255);
+  
+  ledcWrite(0, PWM);
+  ledcWrite(1, PWM);
+  if (motion <= 0) {
     digitalWrite(motorLF, 1);
     digitalWrite(motorLB, 0);
     digitalWrite(motorRF, 1);
     digitalWrite(motorRB, 0);
-  } else if (ax > 100) {
+  } else {
     digitalWrite(motorLF, 0);
     digitalWrite(motorLB, 1);
     digitalWrite(motorRF, 0);
     digitalWrite(motorRB, 1);
-  } else {
-    digitalWrite(motorLF, 0);
-    digitalWrite(motorLB, 0);
-    digitalWrite(motorRF, 0);
-    digitalWrite(motorRB, 0);
   }
 }
-//obtaining a refined value
-float regulator (int error)
-{
-  return error * Pk;
-}
+//PID regulator function
+float PID (int error) {
+  //P
+  float P = error * Pk;
 
+  //I
+  float I = 0;
+
+  //D
+  float D = 0;
+
+  //output
+  return P + I + D;
+}
